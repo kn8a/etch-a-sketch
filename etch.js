336924fd59ba12d3deaa35bb754807b7b50021ec
kn8a <mydreamhomefl@gmail.com>
2022-01-04 00:36:39 -0500

    all basic functions are working

--- etch.js
+++ etch.js
@@ -1,4 +1,15 @@
-const size = 16;
+let gridSize = 0;
+while (gridSize>100 || gridSize<2) {
+    gridSize = Number(prompt("Enter grid size between 2 and 100:"));
+    if (gridSize < 101 && gridSize > 1) {
+        break;
+    }
+    else {
+        alert("You entered size that is out of range, please retry!");
+    }
+}
+
+const size = gridSize;
 
 const grid = document.querySelector(".grid");
 
@@ -12,3 +23,7 @@ for (let h=0; h<size; h++) {
         row.appendChild(column);
     }
 }
+const cells = document.querySelectorAll('.cell');
+cells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => cell.setAttribute('class', 'cell dark'))
+})
